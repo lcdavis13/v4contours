5488b273b0e2fba2a09a6d9bd5490bbd5a4fe9de
lcdavis13 <3443196+lcdavis13@users.noreply.github.com>
2023-11-04 13:42:15 -1000

    functions and improve drawing

--- plotcell2d.py
+++ plotcell2d.py
@@ -1,49 +1,61 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Initialize variables to store data
-contours = []
-current_contour = []
+def read_contours_from_file(file_path):
+    contours = []
+    current_contour = []
 
-# Read the data from the file and process it
-with open("data/GAJ.XY", "r") as file:
-    for line in file:
-        line = line.strip()
-        if line:
-            x, y, depth = map(float, line.split())
-            current_contour.append((x, y, depth))
-        else:
-            if current_contour:
-                contours.append(current_contour)
-            current_contour = []
+    with open(file_path, "r") as file:
+        for line in file:
+            line = line.strip()
+            if line:
+                x, y, depth = map(float, line.split())
+                current_contour.append((x, y, depth))
+            else:
+                if current_contour:
+                    contours.append(current_contour)
+                current_contour = []
 
-# Create a dictionary to store contours by depth
-contours_by_depth = {}
-for contour in contours:
-    depth = contour[0][2]  # Depth of the contour
-    if depth not in contours_by_depth:
-        contours_by_depth[depth] = []
-    contours_by_depth[depth].append(contour)
+    return contours
 
-# Specify the depths you want to filter and plot
-depths_to_plot = [2.0, 3.0, 4.0, 5.0]
+def organize_contours_by_depth(contours):
+    contours_by_depth = {}
+    for contour in contours:
+        depth = contour[0][2]  # Depth of the contour
+        if depth not in contours_by_depth:
+            contours_by_depth[depth] = []
+        contours_by_depth[depth].append(contour)
 
-# Plot the contours with different colors and labels for specified depths
-colors = plt.cm.viridis(np.linspace(0, 1, len(depths_to_plot)))
-legend_handles = []
-for depth, color in zip(depths_to_plot, colors):
-    for contour in contours_by_depth[depth]:
-        contour = np.array(contour)
-        line, = plt.plot(contour[:, 0], contour[:, 1], color=color)
-    legend_handles.append(line)
+    return contours_by_depth
 
-# Add a custom legend with the appropriate colors
-plt.legend(legend_handles, [f"Depth {depth}" for depth in depths_to_plot])
+def plot_contours_with_depths(contours_by_depth, depths_to_plot, legend_handles):
+    colors = plt.cm.viridis(np.linspace(0, 1, len(depths_to_plot)))
+    for depth, color in zip(depths_to_plot, colors):
+        for contour in contours_by_depth[depth]:
+            contour = np.array(contour)
+            line, = plt.plot(contour[:, 0], contour[:, 1], color=color, ls=(0, (5, 20.0/depth)))
+        legend_handles.append(line)
 
-# Set labels and title
-plt.xlabel('X')
-plt.ylabel('Y')
-plt.title('Contour Plots')
+def plot_file(file_path, depths_to_plot, legend_handles):
+    contours = read_contours_from_file(file_path)
+    contours_by_depth = organize_contours_by_depth(contours)
+    
+    plot_contours_with_depths(contours_by_depth, depths_to_plot, legend_handles)
+    
+def show_plots(legend_handles, depths_to_plot):
+    plt.legend(legend_handles, [f"Depth {depth}" for depth in depths_to_plot])
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.title('Contour Plots')
+    plt.show()
 
-# Show the plot
-plt.show()
+
+if __name__ == "__main__":
+    depths_to_plot = [2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0]
+    legend_handles = []
+    
+    plot_file("data/GBE.XY", depths_to_plot, legend_handles)
+    plot_file("data/GAJ.XY", depths_to_plot, legend_handles)
+
+    show_plots(legend_handles, depths_to_plot)
+    
