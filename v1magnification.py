269c984a7874f575de68e3cd64f9474d6d51bf80
lcdavis13 <3443196+lcdavis13@users.noreply.github.com>
2023-11-06 15:12:22 -1000

    v1 magnification working in 2d

--- v1magnification.py
+++ v1magnification.py
@@ -8,138 +8,114 @@ from scipy import optimize
 
 # Define a function to convert polar coordinates to Euclidean coordinates
 def polar_to_euclidean(r, theta):
-    x = r * np.cos(np.radians(theta))
-    y = r * np.sin(np.radians(theta))
+    x = r * np.cos(theta)
+    y = r * np.sin(theta)
     return x, y
 
 # Create an array of angles from 0 to 90 degrees
-angles = np.arange(0, 91, 1)
+dtheta = math.pi/180.0
+angles = np.arange(0.0, math.pi/2.0, dtheta)
 
 # Create lists to store the x and y coordinates of the points on the unit circle
-x_coordinates = [np.cos(np.radians(theta)) for theta in angles]
-y_coordinates = [np.sin(np.radians(theta)) for theta in angles]
+circle_x = np.array([np.cos(theta) for theta in angles])
+circle_y = np.array([np.sin(theta) for theta in angles])
 
 # List of specific angles to plot points at
-specific_angles = [0, 1, 2, 3, 4, 5, 10, 20, 40, 60, 80]
+dot_ids = [0, 1, 2, 5, 10, 20, 40, 60, 80, 89]
+colors = ["black", "red", "blue", "magenta", "cyan", "red", "blue", "magenta", "cyan", "black"]
 
-# Calculate the coordinates for these specific angles on both curves
-specific_coordinates = [polar_to_euclidean(1, theta) for theta in specific_angles]
 
-# Extract x and y coordinates for the specific points
-x_specific, y_specific = zip(*specific_coordinates)
 
-# Plot the unit circle using polar coordinates
-plt.figure(figsize=(12, 4))
-plt.subplot(131)
-plt.plot(x_coordinates, y_coordinates)
-plt.scatter(x_specific, y_specific, marker='o', label='Specific Points')
-plt.title('Unit Circle (Polar Coordinates)')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.grid()
-
-def approximate_derivative(f, z, h=1e-5):
-    """
-    Approximate the derivative of the function f(x) at the point z using the central difference method.
-    :param f: The function to be differentiated.
-    :param z: The point at which to approximate the derivative.
-    :param h: The step size (small positive value).
-    :return: The approximate derivative f'(z).
-    """
-    derivative = (f(z + h) - f(z - h)) / (2 * h)
-    return derivative
+def calc_m(w):
+    # return 716.197 / (71.6197 + w) # radians version from Brad
+    # return 10 / (0.8 + w)  # degrees version from Brad
+    return 525.6 / (1.0 + 33.48*w)  # 1984 radians version
 
 # Define an undefined custom function to convert polar coordinates to Euclidean coordinates
-def Convert(w_deg):
-    w_rad = math.radians(w_deg)
-    
-    # def M_deg(w):
-    #     return 716.197 / (71.6197 + w)
-    # r_deg = M_deg(w_deg)*np.sin(w_rad)
-    
-    
-    def M_rad(w):
-        return 10 / (0.8 + w)
-    def R_rad(w):
-        return M_rad(w)*np.sin(w)
-    r = R_rad(w_rad)
-    
-    def zprime(w, Rfunc):
-        if w == 0.0:
-            return 0.0 # to address rounding errors that lead to a negative number under the square root
-        m = M_rad(w)
-        dr_dw = approximate_derivative(Rfunc, w)
-        sqdif = (m ** 2) - (dr_dw ** 2)
-        return math.sqrt(sqdif)
-    
-    # Deg and rad versions of M equations (given to me by Dr. Motter) are surprisingly a bit different curves
-    if w_rad == 0.0:
-        z = 0.0
-    else:
-        intres = integrate.quad(lambda w: zprime(w, R_rad), 0, w_rad)
-        z, _ = intres
-    
-    #x = np.cos(np.radians(w))
-    #y = np.sin(np.radians(w))
-    return z, r
+def calc_r(w, m):
+    return m * np.sin(w)
+    # might need to ensure these are zero when w is zero
 
+def calc_delta_z(m, r):
+    dr_dw = np.diff(r)/dtheta  # w is theta; dw is dtheta
+    dr_dw = np.append(dr_dw, dr_dw[-1]) # diff loses one element, duplicate last derivative to preserve length
+    m_sqr = np.square(m)
+    dr_dw_sqr = np.square(dr_dw)
+    sqr_dif = np.subtract(m_sqr, dr_dw_sqr)
+    return np.sqrt(sqr_dif)
 
 # Define an undefined custom function to convert polar coordinates to Euclidean coordinates
-def ConvertDebug(w_deg):
-    w_rad = math.radians(w_deg)
-    
-    # def M_deg(w):
-    #     return 716.197 / (71.6197 + w)
-    # r_deg = M_deg(w_deg)*np.sin(w_rad)
-    
-    def M_rad(w):
-        return 10 / (0.8 + w)
-    
-    def R_rad(w):
-        return M_rad(w) * np.sin(w)
-    
-    r = R_rad(w_rad)
+def calc_z(delta_z):
+    return np.cumsum(delta_z)*dtheta
 
-    # x = np.cos(np.radians(w))
-    # y = np.sin(np.radians(w))
-    return w_rad, r
-
-# Create lists to store the x and y coordinates of the points on the unit circle using the custom function
-custom = [Convert(theta) for theta in angles]
-x_custom = [custom_elem[0] for custom_elem in custom]
-y_custom = [custom_elem[1] for custom_elem in custom]
-
-custom2 = [ConvertDebug(theta) for theta in angles]
-x_custom2 = [custom_elem[0] for custom_elem in custom2]
-y_custom2 = [custom_elem[1] for custom_elem in custom2]
+m = calc_m(angles)
+r = calc_r(angles, m)
+delta_z = calc_delta_z(m, r)
+z = calc_z(delta_z)
 
 
-#Also plot some key points
-specific_custom_coordinates = [Convert(theta) for theta in specific_angles]
-x_specific_custom, y_specific_custom = zip(*specific_custom_coordinates)
-
-specific_custom_coordinates2 = [ConvertDebug(theta) for theta in specific_angles]
-x_specific_custom2, y_specific_custom2 = zip(*specific_custom_coordinates2)
-
-# Plot the unit circle using the custom function
-plt.subplot(132)
-plt.plot(x_custom, y_custom)
-plt.scatter(x_specific_custom, y_specific_custom, marker='o', label='Specific Points')
-plt.title('Unit Circle (Custom Function)')
-plt.xlabel('z')
-plt.ylabel('r')
+# Circle
+plt.figure(figsize=(9, 9))
+plt.subplot(321)
+plt.plot(circle_x, circle_y)
+plt.scatter(circle_x[dot_ids], circle_y[dot_ids], c=colors, marker='o', label='Specific Points')
+plt.title('Eye space (unit hemisphere)')
+plt.xlabel('axial distance (x)')
+plt.ylabel('lateral distance (y)')
+plt.xlim(max(circle_x), min(circle_x))
+plt.axis('equal')
 plt.grid()
 
-# Plot the unit circle using the custom function
-plt.subplot(133)
-plt.plot(x_custom2, y_custom2)
-plt.scatter(x_specific_custom2, y_specific_custom2, marker='o', label='Specific Points')
-plt.title('Unit Circle (Custom Function)')
-plt.xlabel('w')
-plt.ylabel('r')
+
+# V1
+plt.subplot(322)
+plt.plot(z, r)
+plt.scatter(z[dot_ids], r[dot_ids], c=colors, marker='o', label='Specific Points')
+plt.title('V1 cortical space')
+plt.xlabel('v1 axial distance (z) (mm)')
+plt.ylabel('v1 lateral distance (r) (mm)')
+plt.axis('equal')
 plt.grid()
 
-print(np.subtract(x_custom, x_custom2))
+# m
+plt.subplot(323)
+plt.plot(angles, m)
+plt.scatter(angles[dot_ids], m[dot_ids], c=colors, marker='o', label='Specific Points')
+plt.title('Magnification factor vs angle (debug view)')
+plt.xlabel('angle (w) (radians)')
+plt.ylabel('magnification factor (m)')
+#plt.axis('equal')
+plt.grid()
+
+# r
+plt.subplot(324)
+plt.plot(angles, r)
+plt.scatter(angles[dot_ids], r[dot_ids], c=colors, marker='o', label='Specific Points')
+plt.title('v1 lateral distance vs angle (debug view)')
+plt.xlabel('angle (w) (radians)')
+plt.ylabel('v1 lateral distance (r)')
+#plt.axis('equal')
+plt.grid()
+
+# dz/dw
+plt.subplot(325)
+plt.plot(angles, delta_z)
+plt.scatter(angles[dot_ids], delta_z[dot_ids], c=colors, marker='o', label='Specific Points')
+plt.title('v1 axial rate of change w.r.t. Angle (debug view)')
+plt.xlabel('angle (w) (radians)')
+plt.ylabel('v1 axial rate (dz/dw)')
+#plt.axis('equal')
+plt.grid()
+
+# z
+plt.subplot(326)
+plt.plot(angles, z)
+plt.scatter(angles[dot_ids], z[dot_ids], c=colors, marker='o', label='Specific Points')
+plt.title('v1 depth vs angle (debug view)')
+plt.xlabel('angle (w) (radians)')
+plt.ylabel('v1 axial distance (z)')
+#plt.axis('equal')
+plt.grid()
 
 plt.tight_layout()
 plt.show()
