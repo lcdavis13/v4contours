cc6ae573d479d747e7ec10335f1d3f10d8735461
lcdavis13 <3443196+lcdavis13@users.noreply.github.com>
2023-11-16 22:31:16 -1000

    mirroring

--- plotcell2d.py
+++ plotcell2d.py
@@ -31,7 +31,7 @@ def organize_contours_by_depth(contours):
 
     return contours_by_depth
 
-def plot_contours_with_depths(contours_by_depth, depths_to_plot, legend_handles):
+def plot_contours_with_depths(contours_by_depth, depths_to_plot, legend_handles, plot_mirrors_too=True):
     colors = plt.cm.viridis(np.linspace(0, 1, len(depths_to_plot)))
     for depth, color in zip(depths_to_plot, colors):
         contours_at_depth = contours_by_depth.get(depth, [])
@@ -41,9 +41,11 @@ def plot_contours_with_depths(contours_by_depth, depths_to_plot, legend_handles)
                 linewidth = 5.0*math.log(depth)
                 gapwidth = 5.0/math.log(depth)
                 line, = plt.plot(contour[:, 0], contour[:, 1], color=color, ls=(0, (linewidth, gapwidth)))
+                if plot_mirrors_too:
+                    line, = plt.plot(-contour[:, 0], contour[:, 1], color=color, ls=(0, (linewidth, gapwidth)))
             legend_handles.append(line)
         
-def plot_contour_centers(contours_by_depth, depths_to_plot, legend_handles):
+def plot_contour_centers(contours_by_depth, depths_to_plot, legend_handles, plot_mirrors_too=True):
     colors = plt.cm.viridis(np.linspace(0, 1, len(depths_to_plot)))
     for depth, color in zip(depths_to_plot, colors):
         contours_at_depth = contours_by_depth.get(depth, [])
@@ -52,21 +54,23 @@ def plot_contour_centers(contours_by_depth, depths_to_plot, legend_handles):
                 contour = np.array(contour)
                 center = np.mean(contour[:, 0:2], axis=0)
                 line, = plt.plot(center[0], center[1], color=color, marker='o')
+                if plot_mirrors_too:
+                    line, = plt.plot(-center[0], center[1], color=color, marker='o')
             legend_handles.append(line)
 
-def plot_file(file_path, depths_to_plot, legend_handles):
+def plot_file(file_path, depths_to_plot, legend_handles, plot_mirrors_too=True):
     contours = read_contours_from_file(file_path)
     contours_by_depth = organize_contours_by_depth(contours)
     
-    #plot_contours_with_depths(contours_by_depth, depths_to_plot, legend_handles)
-    plot_contour_centers(contours_by_depth, [8.0], legend_handles)
+    #plot_contours_with_depths(contours_by_depth, depths_to_plot, legend_handles, plot_mirrors_too)
+    plot_contour_centers(contours_by_depth, [8.0], legend_handles, plot_mirrors_too)
     
     
-def plot_all_files(folder_path, ext, depths_to_plot, legend_handles):
+def plot_all_files(folder_path, ext, depths_to_plot, legend_handles, plot_mirrors_too=True):
     for filename in os.listdir(folder_path):
         if filename.endswith(ext):#  and (filename.startswith('Q') or filename.startswith('R') or filename.startswith('S')):
             file_path = os.path.join(folder_path, filename)
-            plot_file(file_path, depths_to_plot, legend_handles)
+            plot_file(file_path, depths_to_plot, legend_handles, plot_mirrors_too)
 
     
 def show_plots(legend_handles, depths_to_plot):
