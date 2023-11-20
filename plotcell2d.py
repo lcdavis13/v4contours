451d7030aeda391ea79c0d7d03c25ef8bdd8da75
lcdavis13 <3443196+lcdavis13@users.noreply.github.com>
2023-11-19 15:35:02 -1000

    Some improvements for example plots

--- plotcell2d.py
+++ plotcell2d.py
@@ -1,5 +1,6 @@
 import math
 import os
+import random
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -62,31 +63,44 @@ def plot_file(file_path, depths_to_plot, legend_handles, plot_mirrors_too=True):
     contours = read_contours_from_file(file_path)
     contours_by_depth = organize_contours_by_depth(contours)
     
-    #plot_contours_with_depths(contours_by_depth, depths_to_plot, legend_handles, plot_mirrors_too)
-    plot_contour_centers(contours_by_depth, [8.0], legend_handles, plot_mirrors_too)
+    plot_contours_with_depths(contours_by_depth, depths_to_plot, legend_handles, plot_mirrors_too)
+    #plot_contour_centers(contours_by_depth, [8.0], legend_handles, plot_mirrors_too)
     
     
 def plot_all_files(folder_path, ext, depths_to_plot, legend_handles, plot_mirrors_too=True):
     for filename in os.listdir(folder_path):
-        if filename.endswith(ext):#  and (filename.startswith('Q') or filename.startswith('R') or filename.startswith('S')):
+        if filename.endswith(ext) :# and ((filename.startswith('Q') or filename.startswith('R') or filename.startswith('S')) and random.Random().randint(0, 1000) < 10):
             file_path = os.path.join(folder_path, filename)
             plot_file(file_path, depths_to_plot, legend_handles, plot_mirrors_too)
+            
+            
+def add_target_rings(num_rings):
+    radmult = 2
+    # Adding concentric circles with dashed black lines
+    theta = np.linspace(0, 2 * np.pi, 100)  # Points around the circle
+    for i in range(1, num_rings + 1):
+        x = i * np.cos(theta) * radmult
+        y = i * np.sin(theta) * radmult
+        plt.plot(x, y, color='pink', linestyle='dashed', linewidth=1.5)
+        
 
     
 def show_plots(legend_handles, depths_to_plot):
-    plt.legend(legend_handles, [f"Depth {depth}" for depth in depths_to_plot])
+    plt.legend(legend_handles, [f"Depth {depth}" for depth in depths_to_plot], loc='upper left')
     plt.xlabel('X')
     plt.ylabel('Y')
-    plt.title('Contour Plots')
+    plt.title('V4 Receptive Fields')
+    plt.axis('equal')
     plt.show()
 
 
 if __name__ == "__main__":
-    depths_to_plot = [2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0]
+    depths_to_plot = [3.0, 4.0, 5.0, 6.0, 7.0, 8.0] #[2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0]
     legend_handles = []
     
     #plot_file('data/JDK.XY', depths_to_plot, legend_handles)
-    plot_all_files('data', '.XY', depths_to_plot, legend_handles)
+    plot_all_files('data', '.XY', depths_to_plot, legend_handles, plot_mirrors_too=False)
+    add_target_rings(10)
 
     show_plots(legend_handles, depths_to_plot)
     
